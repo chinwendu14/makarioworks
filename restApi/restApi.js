6ae4bf1c7fa5346805ca1841c815ab002491e131
user name <user email>
2023-04-28 09:40:30 +0100

    api request

--- restApi/restApi.js
+++ restApi/restApi.js
@@ -5,7 +5,7 @@ const API_key = "4dbedf89c7864b1b6e8bf771a6be21de";
 export const getwhether = async ({ city }) => {
   try {
     const response = await axios.get(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&id=524901&appid=${API_key}`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&id=524901&appid=${API_key}`
     );
     const res = response.data;
     return res;
